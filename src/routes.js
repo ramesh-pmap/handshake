f3df452ccfc1a9236bd5dc297865e433d1c34ea7
Michel Marrache <mmarrache@processmap.com>
2016-01-06 18:26:32 -0500

    Testing iframe component

--- src/routes.js
+++ src/routes.js
@@ -8,14 +8,16 @@ import LoginPage from './modules/global/pages/login';
 import HomePage from './modules/global/pages/home';
 import DataPage from './modules/global/pages/data';
 import DocMgtPage from './modules/document-management/home';
+import Iframe from './modules/global/pages/iframe';
 
 
 // Routes template.
 export default (
 	<Router history={createBrowserHistory()}>
-		<Route path="/" component={LoginPage} title="Login Page" />
-		<Route path="/home" component={HomePage} title="Home" />
-		<Route path="data" component={DataPage} title="Data Sample Page" />
-		<Route path="document-management" component={DocMgtPage} title="Document Management" />
+		<Route path="/react/" component={LoginPage} title="Login Page" />
+		<Route path="/react/home" component={HomePage} title="Home" />
+		<Route path="/react/data" component={DataPage} title="Data Sample Page" />
+		<Route path="/react/document-management" component={DocMgtPage} title="Document Management" />
+		<Route path="/react/iframe" component={Iframe} title="iframe Demo" />
 	</Router>
 );
