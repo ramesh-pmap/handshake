f03f23164a313b24e85f904ea53fe5272a8d81fa
Michel Marrache <mmarrache@processmap.com>
2016-01-18 18:02:07 -0500

    Added picklst (react-select) component/package. Need to finish Detail Form view

--- src/modules/document-management/components/RightPanelArea/DetailForm.js
+++ src/modules/document-management/components/RightPanelArea/DetailForm.js
@@ -4,9 +4,11 @@ import { connect } from 'react-redux';
 import { setRightPanelAreaView } from '../../../../redux/actions';
 import { DETAIL } from '../../../../redux/constants';
 
-// Core components.
+// Components.
 import {Button, Input, FormControls} from 'react-bootstrap';
 // import Icon from 'react-fa';
+import Select from 'react-select';
+
 
 // Define class.
 class DetailForm extends React.Component {
@@ -23,6 +25,25 @@ class DetailForm extends React.Component {
 
   // Render method.
   render() {
+    const employeePicklistOptions = [
+      { value: 'John Smith', label: 'John Smith', clearableValue: false },
+      { value: 'Bob Roberts', label: 'Bob Roberts', clearableValue: false },
+      { value: 'Janette Walls', label: 'Janette Walls', clearableValue: false }
+    ];
+    const docTypePicklistOptions = [
+      { value: 'Document', label: 'Document'},
+      { value: 'Images', label: 'Images'},
+      { value: 'Photo', label: 'Photo'},
+      { value: 'Spreadsheet', label: 'Spreadsheet'}
+    ];
+    const docLevelPicklistOptions = [
+      { value: '1', label: 'Level 1'},
+      { value: '2', label: 'Level 2'},
+      { value: '3', label: 'Level 3'},
+      { value: '4', label: 'Level 4'}
+    ];
+    const selectNoResultsText = 'Sorry, the name you type cannot be found.';
+
     return (
       <div>
         <div className="fixed-title clearfix">
@@ -36,57 +57,88 @@ class DetailForm extends React.Component {
 
         <div className="sidebar-details">
           <div className="document-details">
-            <Input type="text" label="Title" labelClassName=" required" wrapperClassName="" />
-            <Input type="text" label="Internal Document ID" labelClassName="" wrapperClassName="" />
-            <Input type="number" label="Version" labelClassName=" required" wrapperClassName="" />
-            <Input type="select" multiple label="Document Owner" labelClassName=" required" wrapperClassName="">
-              <option>John Smith</option>
-              <option>Bob Roberts</option>
-              <option>Janette Walls</option>
-            </Input>
-            <Input type="select" multiple label="Author" labelClassName="" wrapperClassName="">
-              <option>John Smith</option>
-              <option>Bob Roberts</option>
-              <option>Janette Walls</option>x
-            </Input>
-            <FormControls.Static label="Uploaded/Referenced By" labelClassName="" wrapperClassName="" value="John Doe"/>
-            <FormControls.Static label="Date Uploaded/Referenced" labelClassName="" wrapperClassName="" value="11/7/2015"/>
-            <FormControls.Static label="Modified By" labelClassName="" wrapperClassName="" value="John Doe"/>
-            <FormControls.Static label="Modified Date" labelClassName="" wrapperClassName="" value="11/8/2015"/>
-            <FormControls.Static label="Document Status" labelClassName="" wrapperClassName="" value="Pending Approval"/>
-            <Input type="select" label="Document Type" labelClassName="" wrapperClassName="">
-              <option>Document</option>
-              <option>Images</option>
-              <option>Photo</option>
-              <option>Spreadsheet</option>
-            </Input>
-            <Input type="select" label="Document Level" labelClassName=" required" wrapperClassName="">
-              <option>Level 1</option>
-              <option>Level 2</option>
-              <option>Level 3</option>
-              <option>Level 4</option>
-            </Input>
-            <Input type="select" multiple label="Display Folders" labelClassName=" required" wrapperClassName="">
+            <Input type="text" label="Title" labelClassName=" required" />
+            <Input type="text" label="Internal Document ID" />
+            <Input type="number" label="Version" labelClassName=" required" />
+
+            <div className="form-group">
+              <label className="control-label">Document Owner</label>
+              <Select
+                name="form-control"
+                placeholder="Type to find Document Owner"
+                noResultsText={selectNoResultsText}
+                value=""
+                options={employeePicklistOptions}
+                // multi
+                // isLoading
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="control-label">Author</label>
+              <Select
+                name="form-control"
+                placeholder="Type to find Author"
+                noResultsText={selectNoResultsText}
+                value=""
+                options={employeePicklistOptions}
+              />
+            </div>
+
+            <FormControls.Static label="Uploaded/Referenced By" value="John Doe"/>
+            <FormControls.Static label="Date Uploaded/Referenced" value="11/7/2015"/>
+            <FormControls.Static label="Modified By" value="John Doe"/>
+            <FormControls.Static label="Modified Date" value="11/8/2015"/>
+            <FormControls.Static label="Document Status" value="Pending Approval"/>
+
+            <div className="form-group">
+              <label className="control-label">Document Type</label>
+              <Select
+                name="form-control"
+                placeholder="Type to find Document Type"
+                noResultsText={selectNoResultsText}
+                value=""
+                options={docTypePicklistOptions}
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="control-label">Document Level</label>
+              <Select
+                name="form-control"
+                placeholder="Type to find Document Level"
+                noResultsText={selectNoResultsText}
+                value=""
+                options={docLevelPicklistOptions}
+              />
+            </div>
+
+            <Input type="select" multiple label="Display Folders" labelClassName=" required">
               <option>1.01 Incident and Emergency Management</option>
               <option>2.0 Quality Management System</option>
             </Input>
-            <Input type="textarea" label="Description" labelClassName="" wrapperClassName="" />
-            <Input type="textarea" label="Keywords/Tags" labelClassName="" wrapperClassName="" />
-            <Input type="select" multiple label="Reference Documents" labelClassName="" wrapperClassName="">
+
+            <Input type="textarea" label="Description" />
+            <Input type="textarea" label="Keywords/Tags" />
+
+            <Input type="select" multiple label="Reference Documents">
               <option>Incident Management</option>
               <option>Incident Investigation</option>
             </Input>
-            <Input type="textarea" label="Regulatory References" labelClassName="" wrapperClassName="" />
-            <FormControls.Static label="Review Date" labelClassName="" wrapperClassName="" value="12/20/2015"/>
-            <Input type="textarea" label="Review Date" labelClassName="" wrapperClassName="" />
-            <Input type="select" label="Retention Period" labelClassName="" wrapperClassName="">
+
+            <Input type="textarea" label="Regulatory References" />
+            <FormControls.Static label="Review Date" value="12/20/2015"/>
+            <Input type="textarea" label="Review Date" />
+
+            <Input type="select" label="Retention Period">
               <option>Option1</option>
               <option>Option2</option>
               <option>Option3</option>
             </Input>
-            <Input type="textarea" label="Reason for Change" labelClassName="" wrapperClassName="" />
-            <FormControls.Static label="File Name" labelClassName="" wrapperClassName="" value="OSHA_V2.0.docx"/>
-            <FormControls.Static label="File Size" labelClassName="" wrapperClassName="" value="2.5 MB"/>
+
+            <Input type="textarea" label="Reason for Change" />
+            <FormControls.Static label="File Name" value="OSHA_V2.0.docx"/>
+            <FormControls.Static label="File Size" value="2.5 MB"/>
           </div>
         </div>
       </div>
