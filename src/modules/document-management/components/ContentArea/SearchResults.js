20b254e75d784795566a5786cc05fff2231ed868
Michel Marrache <mmarrache@processmap.com>
2016-01-20 16:41:28 -0500

    Added basic search results view

--- src/modules/document-management/components/ContentArea/SearchResults.js
+++ src/modules/document-management/components/ContentArea/SearchResults.js
@@ -2,6 +2,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+// Core components.
+import {Row, Col, Button, ListGroup, ListGroupItem} from 'react-bootstrap';
+import Icon from 'react-fa';
+
 
 // Define class.
 class SearchResults extends React.Component {
@@ -10,12 +14,84 @@ class SearchResults extends React.Component {
     super(props);
   }
 
+  hangleIconCheckChange(e) {
+    let isChecked = e.target.checked;
+    console.log(isChecked);
+    // e.target.dataset.checked = !isChecked;
+  }
 
   // Render method.
   render() {
     return (
-      <div>
-        <h1>Search results page</h1>
+      <div className="card">
+        <div className="file_manager-list_header">
+
+          <Row className="file_manager-list_titles hidden-xs">
+            <Col sm={6}>
+              <input type="checkbox" onClick={this.hangleIconCheckChange.bind(this)} />
+              &nbsp; &nbsp;
+              <b>Type &nbsp;Name</b>
+            </Col>
+            <Col sm={1} className="text-center">
+              <b>Version</b>
+            </Col>
+            <Col sm={2} className="text-center">
+              <b>Modified</b>
+            </Col>
+            <Col sm={3} className="text-center">
+              <b>Status</b>
+            </Col>
+          </Row>
+
+          <ListGroup className="file_manager-list">
+
+            <ListGroupItem>
+              <Row>
+                <Col sm={6}>
+                  <input type="checkbox" onClick={this.hangleIconCheckChange.bind(this)} />
+                  &nbsp;&nbsp;
+                  <Button href="#/" bsStyle="link" bsSize="xs">
+                    <Icon name="folder-open" className="fa-fw fa-lg text-muted" />
+                    &nbsp;
+                    Folder Name
+                  </Button>
+                </Col>
+                <Col sm={1} className="text-center hidden-xs" />
+                <Col sm={2} className="text-center text-left-xs">
+                  <span className="visible-xs-inline">Date Modified: </span>
+                  12/3/2015
+                </Col>
+              </Row>
+            </ListGroupItem>
+
+            <ListGroupItem>
+              <Row>
+                <Col sm={6}>
+                  <input type="checkbox" onClick={this.hangleIconCheckChange.bind(this)} />
+                  &nbsp;&nbsp;
+                  <Button href="#/" bsStyle="link" bsSize="xs">
+                    <Icon name="file-word-o" className="fa-fw fa-lg text-muted" />
+                    &nbsp;
+                    File Name
+                  </Button>
+                </Col>
+                <Col sm={1} className="text-center text-left-xs">
+                  <span className="visible-xs-inline">Version: </span>
+                  <Button href="#" bsStyle="link" bsSize="xs">1</Button>
+                </Col>
+                <Col sm={2} className="text-center text-left-xs">
+                  <span className="visible-xs-inline">Date Modified: </span>
+                  12/3/2015
+                </Col>
+                <Col sm={3} className="text-center text-left-xs">
+                  Status
+                </Col>
+              </Row>
+            </ListGroupItem>
+
+          </ListGroup>
+
+        </div>
       </div>
     );
   }
