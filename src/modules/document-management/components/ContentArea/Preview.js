348ddacddf56f34f862fede69c02b1e15f31efb1
Michel Marrache <mmarrache@processmap.com>
2016-01-21 22:34:50 -0500

    Added Acknowledgement component to static Preview content area view

--- src/modules/document-management/components/ContentArea/Preview.js
+++ src/modules/document-management/components/ContentArea/Preview.js
@@ -1,7 +1,7 @@
 // Dependencies.
 import React from 'react';
 import { connect } from 'react-redux';
-import { ButtonGroup, Button, Image } from 'react-bootstrap';
+import { ButtonGroup, Button, Image, Alert } from 'react-bootstrap';
 import Icon from 'react-fa';
 
 // Redux.
@@ -18,6 +18,10 @@ class Preview extends React.Component {
   constructor(props) {
     // Pass `props` into scope.
     super(props);
+
+    this.state = {
+      alertVisible: true
+    };
   }
 
   closePreview() {
@@ -25,26 +29,52 @@ class Preview extends React.Component {
     dispatch(setContentAreaView(DEFAULT));
   }
 
+  // !!!!!!!!!!!!!!!!!!!!
+  // !!! DON'T DELETE !!!
+  // !!!!!!!!!!!!!!!!!!!!
   print() {
-    const printableContent = document.querySelector('#printableContent');
-    const pri = printableContent.contentWindow;
-    pri.document.open();
-    pri.document.write(printableContent.innerHTML);
-    pri.document.close();
-    pri.focus();
-    pri.print();
+  //   const printableContent = document.querySelector('#printableContent');
+  //   const pri = printableContent.contentWindow;
+  //   pri.document.open();
+  //   pri.document.write(printableContent.innerHTML);
+  //   pri.document.close();
+  //   pri.focus();
+  //   pri.print();
   }
+  // !!!!!!!!!!!!!!!!!!!!
 
   showDetail() {
-    const { state, dispatch } = this.props;
+    const { dispatch } = this.props;
     dispatch(setRightPanelAreaView(DETAIL));
     dispatch(toggleRightSidebar(false));
-    console.log(state.rightSidebarOpened);
+    // console.log(state.rightSidebarOpened);
+  }
+
+  iAcknowledgeThisDocument() {
+    this.setState({ alertVisible: false });
+    alert('Thank you.');
   }
 
   // Render method.
   render() {
     const { state } = this.props;
+
+    let acknowledgementAlert = '';
+    if (this.state.alertVisible) {
+      acknowledgementAlert = (
+        <div className="acknowledgement-wrapper">
+          <Alert bsStyle="info" className="text-center">
+            <p>This document requires that you acknowledge that you read it in its entirety.</p>
+            <br/>
+            <p>
+              <Button bsStyle="primary" onClick={this.iAcknowledgeThisDocument.bind(this)}>
+                I acknowledge this document
+              </Button>
+            </p>
+          </Alert>
+        </div>
+      );
+    }
 
     return (
       <div className="preview-panel" style={{height: state.windowDimensions.height - 88}}>
@@ -81,14 +111,10 @@ class Preview extends React.Component {
               Close
             </Button>
           </ButtonGroup>
-          <div className="pull-right">
-          </div>
         </div>
         <Image src="/static/images/sample-doc-preview.png" />
 
-        <iframe id="printableContent" style={{position: 'absolute', top: 0, left: 0, height: 0, width: 0}}>
-          <Image src="/static/images/sample-doc-preview.png" />
-        </iframe>
+        {acknowledgementAlert}
 
       </div>
     );
