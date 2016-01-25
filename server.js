5528323b604120424f70954c93cd0f4f15768bb6
Alessandro Barroso <abarroso@processmap.com>
2016-01-25 17:14:19 -0300

    code to use ES6+ features on node<4.0

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const r = require('rethinkdb');
+import bodyParser from 'body-parser';
+import express from 'express';
+import r from 'rethinkdb';
 
 
 const app = express();
