6d4761ebad2388d8bb0e037c4ec4fb6cfad4c9bb
Alessandro Barroso <abarroso@processmap.com>
2016-02-02 21:48:54 -0300

    adding graphql layer

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import graphHTTP from 'express-graphql';
+import Schema from './server/api/schema';
 
 import * as api from './server/api/http';
 
@@ -20,8 +22,16 @@ app.use(express.static('build'));
 /**
  * API Endpoints
  */
-
 app.get('/api/0/documents', api.getDocuments);
 
+/**
+ * GraphQL Query
+ */
+app.use('/graphql', graphHTTP({
+  schema: Schema,
+  pretty: true,
+  graphiql: true
+}));
 
-app.listen(port, () => console.log('Rest API server running on http://cosmos.pmapconnect.com:' + port));
+
+app.listen(port, () => console.log('Rest API server running on http://localhost:' + port));
