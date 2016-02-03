88a1637173f322a8aecfcec37074b63f8ebae3f3
Michel Marrache <mmarrache@processmap.com>
2016-02-03 12:14:12 -0500

    Updated Business Process options

--- src/modules/document-management/components/ContentArea/FileManager/FileManager.js
+++ src/modules/document-management/components/ContentArea/FileManager/FileManager.js
@@ -2,11 +2,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { Scrollbars } from 'react-custom-scrollbars';
-
 // Core components.
-import { Button } from 'react-bootstrap';
-import Icon from 'react-fa';
+// import { Button } from 'react-bootstrap';
+// import Icon from 'react-fa';
 
 // Components
 import FileManagerBreadcrumb from './FileManagerBreadcrumb';
@@ -38,23 +36,6 @@ class FileManager extends React.Component {
   // Render method.
   render() {
     const { foldersData, filesData, breadcrumbData, currentFolderId } = this.props;
-    const { height } = this.state;
-
-    let tableRows;
-    switch (height) {
-    case AUTO_HEIGHT:
-      tableRows = (
-          <FileManagerRows foldersData={foldersData} filesData={filesData} currentFolderId={currentFolderId}/>
-      );
-      break;
-    default:
-      tableRows = (
-        <Scrollbars style={{ height: [height] }}>
-        {/* File Manager Rows */}
-        <FileManagerRows foldersData={foldersData} filesData={filesData} currentFolderId={currentFolderId}/>
-        </Scrollbars>
-      );
-    }
 
     return (
       <div>
@@ -68,24 +49,10 @@ class FileManager extends React.Component {
 
         <div className="card" style={{ position: 'relative' }}>
 
-        <div style={{ display: 'none', position: 'absolute', top: -10, right: -10 }}>
-            <small className="text-muted">Freeze Table Header</small>
-            <Button bsSize="lg" bsStyle="link" className="hidden-xs"
-              onClick={this.handleFixedHeaderToggle.bind(this)}>
-              {
-                  height === AUTO_HEIGHT ?
-                    <Icon name="toggle-off" />
-                  :
-                    <Icon name="toggle-on" />
-
-              }
-            </Button>
-          </div>
-
           {/* File Manager Header */}
           <FileManagerHeader />
 
-          { tableRows }
+          <FileManagerRows foldersData={foldersData} filesData={filesData} currentFolderId={currentFolderId}/>
 
         </div>
 
